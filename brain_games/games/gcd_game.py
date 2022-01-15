b3c221194794f4c3b4a20956d735f1b4659038bf
ilnur.nasibullin <nasosurulya@gmail.com>
2022-01-15 22:46:52 +0500

    linter fix

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -8,10 +8,8 @@ def get_gcd(num1, num2):
     else:
         big_num = num2
         small_num = num1
-    
     if big_num % small_num == 0:
         return small_num
-    
     i = small_num // 2
     while i > 0:
         if big_num % i == 0 and small_num % i == 0:
@@ -24,7 +22,7 @@ def has_win_game():
     i = 0
     while i < games.ROUND_COUNT:
         num1 = games.generate_random_number()
-        num2 = games.generate_random_number()        
+        num2 = games.generate_random_number()
         answer = games.get_answer('{} {}'.format(num1, num2))
         correct_answer = get_gcd(num1, num2)
         if not games.is_answer_correct(answer, str(correct_answer)):
