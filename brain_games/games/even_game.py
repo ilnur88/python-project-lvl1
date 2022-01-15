4e51bcd339b1c6982d910646de46a13e6f7897f6
ilnur.nasibullin <nasosurulya@gmail.com>
2022-01-15 23:13:12 +0500

    fix

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -13,7 +13,7 @@ def has_win_game():
     i = 0
     while i < games_universal.ROUND_COUNT:
         num = games_universal.generate_random_number()
-        answer = games_universal.ask_question_and_get_answer(num)
+        answer = games_universal.get_answer(num)
         correct_answer = check_number_is_even(num)
         if not games_universal.is_answer_correct(answer, correct_answer):
             return False
