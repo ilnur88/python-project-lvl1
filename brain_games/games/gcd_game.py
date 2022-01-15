003a203e319099cd23cb53bd0fb87f4eb28a5a36
ilnur.nasibullin <nasosurulya@gmail.com>
2022-01-15 21:30:56 +0500

    seventh step

---
+++ brain_games/games/gcd_game.py
@@ -0,0 +1,33 @@
+import brain_games.games.games_universal as games
+
+
+def get_gcd(num1, num2):
+    if num1 > num2:
+        big_num = num1
+        small_num = num2
+    else:
+        big_num = num2
+        small_num = num1
+    
+    if big_num % small_num == 0:
+        return small_num
+    
+    i = small_num // 2
+    while i > 0:
+        if big_num % i == 0 and small_num % i == 0:
+            return i
+        i = i - 1
+
+
+def has_win_game():
+    print('What is the result of the expression?')
+    i = 0
+    while i < games.ROUND_COUNT:
+        num1 = games.generate_random_number()
+        num2 = games.generate_random_number()        
+        answer = games.get_answer('{} {}'.format(num1, num2))
+        correct_answer = get_gcd(num1, num2)
+        if not games.is_answer_correct(answer, str(correct_answer)):
+            return False
+        i = i + 1
+    return True
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-from brain_games.games.calc_game import has_win_game
+from brain_games.games.gcd_game import has_win_game
 from brain_games.cli import welcome_user
 
 
 def main():
     name = welcome_user()
-    print('What is the result of the expression?')
+    print('Find the greatest common divisor of given numbers.')
     if has_win_game():
         print('Congratulations, {}!'.format(name))
     else:
